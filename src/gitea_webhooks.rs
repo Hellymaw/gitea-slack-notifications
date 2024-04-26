0e4da2132b22ef011462252fbdb23e8fada6c467
Hellymaw <aaron@helmore.org>
2024-04-26 17:08:01 +1000

    working pipeline

--- src/gitea_webhooks.rs
+++ src/gitea_webhooks.rs
@@ -1,5 +1,8 @@
+use reqwest::Client;
 use serde::{Deserialize, Serialize};
+use serde_json::{self, json};
 use slack_morphism::prelude::*;
+use strum::Display;
 
 #[derive(Deserialize, Debug)]
 pub struct User {
@@ -30,8 +33,9 @@ pub struct PullRequest {
     pub state: PullRequestState,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Display)]
 #[serde(tag = "type")]
+#[strum(serialize_all = "snake_case")]
 pub enum Review {
     #[serde(rename = "pull_request_review_approved")]
     Approved { content: String },
@@ -41,13 +45,15 @@ pub enum Review {
     Comment { content: String },
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Display)]
 #[serde(rename_all = "snake_case", tag = "action")]
+#[strum(serialize_all = "snake_case")]
 pub enum Action {
     Opened,
     Closed,
     Reopened,
     Edited,
+    Merged,
     Reviewed { review: Review },
     ReviewRequested { requested_reviewer: User },
 }
@@ -68,12 +74,141 @@ pub struct OutgoingWebhook {
     pub body: String,
 }
 
-impl SlackMessageTemplate for Webhook {
-    fn render_template(&self) -> SlackMessageContent {
-        match self.action {
-            Action::Opened => render_pr_opened(self),
-            _ => SlackMessageContent::new().with_text(format!("A new PR action occurred")),
+pub struct MySlackMessage<'a> {
+    pub webhook: &'a Webhook,
+    pub slack_user: Option<SlackUser>,
+}
+
+const GITEA_ADDRESS: &str = "http://localhost:3000/api/v1";
+
+impl Webhook {
+    pub async fn deanonymise_emails(
+        mut self,
+    ) -> Result<Self, Box<dyn std::error::Error + Send + Sync>> {
+        self.sender.email = Webhook::fetch_gitea_user_email(&self.sender).await?;
+
+        self.pull_request.user.email =
+            Webhook::fetch_gitea_user_email(&self.pull_request.user).await?;
+
+        if let Action::ReviewRequested {
+            ref mut requested_reviewer,
+        } = self.action
+        {
+            requested_reviewer.email = Webhook::fetch_gitea_user_email(&requested_reviewer).await?;
         }
+
+        Ok(self)
+    }
+
+    async fn fetch_gitea_user_email(
+        user: &User,
+    ) -> Result<String, Box<dyn std::error::Error + Send + Sync>> {
+        let token = std::env::var("GITEA_API_TOKEN")?;
+        let url = format!("{}/users/{}", GITEA_ADDRESS, user.username);
+
+        let res = Client::new()
+            .get(url)
+            .header("Authorization", "token ".to_string() + &token.to_owned())
+            .send()
+            .await?
+            .json::<User>()
+            .await?;
+
+        Ok(res.email)
+    }
+
+    pub async fn into_my_slack(&self) -> MySlackMessage {
+        let slack_user = match self.action {
+            Action::ReviewRequested {
+                ref requested_reviewer,
+            } => fetch_slack_user_from_email(&requested_reviewer.email)
+                .await
+                // .inspect_err(|e| eprintln!("failed finding user: {e}"))
+                .ok(),
+            Action::Reviewed { review: _ } => {
+                fetch_slack_user_from_email(&self.pull_request.user.email)
+                    .await
+                    // .inspect_err(|e| eprintln!("failed finding user: {e}"))
+                    .ok()
+            }
+            _ => None,
+        };
+
+        MySlackMessage {
+            webhook: self,
+            slack_user,
+        }
+    }
+}
+
+impl SlackMessageTemplate for MySlackMessage<'_> {
+    fn render_template(&self) -> SlackMessageContent {
+        match &self.webhook.action {
+            Action::Opened => render_pr_opened(&self.webhook),
+            Action::Reviewed { review } => render_reviewed(self, review),
+            Action::ReviewRequested { requested_reviewer } => {
+                render_review_requested(self, &requested_reviewer)
+            }
+            _ => render_basic_action(&self.webhook),
+        }
+    }
+}
+
+fn format_pull_request_url(pull_request: &PullRequest) -> String {
+    format!("<{}|{}>", pull_request.url, pull_request.title)
+}
+
+fn render_basic_action(webhook: &Webhook) -> SlackMessageContent {
+    SlackMessageContent::new().with_blocks(slack_blocks![some_into(
+        SlackSectionBlock::new().with_text(md!(
+            "{} was {}",
+            format_pull_request_url(&webhook.pull_request),
+            webhook.action
+        ))
+    )])
+}
+
+fn render_reviewed(slack_message: &MySlackMessage, review: &Review) -> SlackMessageContent {
+    if let Some(user) = &slack_message.slack_user {
+        SlackMessageContent::new().with_blocks(slack_blocks![some_into(
+            SlackSectionBlock::new().with_text(md!(
+                "{}, {} has {} your PR",
+                user.id.to_slack_format(),
+                slack_message.webhook.sender.username,
+                review
+            ))
+        )])
+    } else {
+        SlackMessageContent::new().with_blocks(slack_blocks![some_into(
+            SlackSectionBlock::new().with_text(md!(
+                "{}, {} has {} your PR",
+                slack_message.webhook.pull_request.user.username,
+                slack_message.webhook.sender.username,
+                review
+            ))
+        )])
+    }
+}
+
+fn render_review_requested(slack_message: &MySlackMessage, reviewer: &User) -> SlackMessageContent {
+    if let Some(user) = &slack_message.slack_user {
+        SlackMessageContent::new().with_blocks(slack_blocks![some_into(
+            SlackSectionBlock::new().with_text(md!(
+                "{}, {} has requested you to review {}",
+                user.id.to_slack_format(),
+                slack_message.webhook.sender.username,
+                format_pull_request_url(&slack_message.webhook.pull_request)
+            ))
+        )])
+    } else {
+        SlackMessageContent::new().with_blocks(slack_blocks![some_into(
+            SlackSectionBlock::new().with_text(md!(
+                "{}, {} has requested you to review {}",
+                reviewer.username,
+                slack_message.webhook.sender.username,
+                format_pull_request_url(&slack_message.webhook.pull_request)
+            ))
+        )])
     }
 }
 
@@ -91,11 +226,34 @@ fn render_pr_opened(webhook: &Webhook) -> SlackMessageContent {
             repo_name.1
         ))),
         some_into(SlackSectionBlock::new().with_text(md!(
-            "Pull request <{}|{}> opened by {}",
-            webhook.pull_request.url,
-            webhook.pull_request.title,
+            "Pull request {} opened by {}",
+            format_pull_request_url(&webhook.pull_request),
             webhook.sender.username
         ))),
         some_into(SlackSectionBlock::new().with_text(md!(">{}", webhook.pull_request.body)))
     ])
+}
+
+pub fn config_env_var(name: &str) -> Result<String, String> {
+    std::env::var(name).map_err(|e| format!("{}: {}", name, e))
+}
+
+async fn fetch_slack_user_from_email(
+    email: &str,
+) -> Result<SlackUser, Box<dyn std::error::Error + Send + Sync>> {
+    let client = SlackClient::new(SlackClientHyperConnector::new()?);
+    let token_value: SlackApiTokenValue = config_env_var("SLACK_TEST_TOKEN")?.into();
+    let token: SlackApiToken = SlackApiToken::new(token_value);
+    let session = client.open_session(&token);
+
+    let email = EmailAddress(email.to_string());
+    println!("email: {:?}", email);
+
+    let request = SlackApiUsersLookupByEmailRequest::new(email);
+    let slack_user = session.users_lookup_by_email(&request).await;
+    println!("users: {:?}", slack_user);
+
+    let slack_user = slack_user?;
+
+    Ok(slack_user.user)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
+use axum::body;
+use axum::http::request;
 use axum::{extract::Json, extract::State, routing::post, Router};
-use gitea_webhooks::{Action, OutgoingWebhook, Review, User, Webhook};
-use serde_json;
+use gitea_webhooks::config_env_var;
+use gitea_webhooks::Action;
+use gitea_webhooks::MySlackMessage;
+use gitea_webhooks::User;
+use gitea_webhooks::Webhook;
+
+use serde::Deserialize;
+use serde_json::{self, json};
 use slack_morphism::prelude::*;
 
 pub mod gitea_webhooks;
@@ -10,12 +18,14 @@ use std::sync::{Arc, Mutex};
 
 #[derive(Default, Debug)]
 pub struct AppState {
-    pub cache: HashMap<String, SlackTs>,
+    pub slack_message_cache: HashMap<String, SlackTs>,
+    pub slack_user_lookup: HashMap<String, String>,
 }
 
 pub type SharedState = Arc<Mutex<AppState>>;
 
 const BIND_ADDRESS: &str = "192.168.0.26:6969";
+const GITEA_ADDRESS: &str = "localhost:3000";
 
 #[tokio::main(flavor = "multi_thread")]
 async fn main() {
@@ -31,90 +41,52 @@ async fn main() {
 
 async fn post_handler(State(state): State<SharedState>, Json(payload): Json<serde_json::Value>) {
     match serde_json::from_value::<Webhook>(payload) {
-        Ok(webhook) => match webhook.action {
-            Action::ReviewRequested {
-                requested_reviewer: ref reviewer,
-            } => review_requested(&webhook, &reviewer, state).await,
-            Action::Reviewed { ref review } => reviewed(&webhook, &review, state).await,
-            // Action::Closed => opened(webhook, state).await,
-            Action::Opened => opened(&webhook, state).await,
-            action => println!("Unhandled action \"{:?}\"", action),
-        },
+        Ok(webhook) => post_repo_payload(webhook, state).await,
         Err(x) => println!("{}", x),
     }
 }
 
-async fn review_requested(payload: &Webhook, reviewer: &User, state: SharedState) {
-    let requester = &payload.sender.email;
+async fn post_repo_payload(payload: Webhook, state: SharedState) {
+    let payload = payload.deanonymise_emails().await.unwrap();
+    println!("webhook: {:?}", payload);
 
-    let body = format!("{} requested a review from {}", requester, reviewer.email);
-
-    println!("{body}");
-
-    post_repo_payload(payload, &body, state).await;
-}
-
-async fn reviewed(payload: &Webhook, review: &Review, state: SharedState) {
-    let reviewer = &payload.sender.email;
-
-    let body = format!("{} {:?} the pull-request", reviewer, review);
-
-    println!("{body}");
-
-    post_repo_payload(payload, &body, state).await;
-}
-
-async fn opened(payload: &Webhook, state: SharedState) {
-    let outgoing = OutgoingWebhook {
-        email: payload.sender.email.to_owned(),
-        title: "opened PR#".to_owned(),
-        body: "".to_owned(),
-    };
-
-    let body = serde_json::to_string(&outgoing).unwrap();
-    println!("{:?}", body);
-
-    post_repo_payload(payload, &body, state).await;
-}
-
-pub fn config_env_var(name: &str) -> Result<String, String> {
-    std::env::var(name).map_err(|e| format!("{}: {}", name, e))
-}
-
-async fn post_repo_payload(payload: &Webhook, body: &str, state: SharedState) {
     let ts = {
         let state_data = state.lock().unwrap();
         state_data
-            .cache
+            .slack_message_cache
             .get(&payload.pull_request.url)
             .map(|ts| ts.clone())
     };
 
-    if let Ok(response) = post_slack_message(payload, ts).await {
-        let mut state_data = state.lock().unwrap();
-        state_data
-            .cache
-            .entry(payload.pull_request.url.clone())
-            .or_insert(response);
+    let response = post_slack_message(&payload, &ts).await;
+    if ts.is_none() {
+        if let Ok(response) = response {
+            let mut state_data = state.lock().unwrap();
+            state_data
+                .slack_message_cache
+                .entry(payload.pull_request.url.clone())
+                .or_insert(response);
 
-        println!("state_data: {state_data:?}");
+            println!("Added new entry: {:?}", state_data.slack_message_cache);
+        }
     }
 }
 
 async fn post_slack_message(
     message: &Webhook,
-    parent: Option<SlackTs>,
+    parent: &Option<SlackTs>,
 ) -> Result<SlackTs, Box<dyn std::error::Error + Send + Sync>> {
     let client = SlackClient::new(SlackClientHyperConnector::new()?);
     let token_value: SlackApiTokenValue = config_env_var("SLACK_TEST_TOKEN")?.into();
     let token: SlackApiToken = SlackApiToken::new(token_value);
     let session = client.open_session(&token);
 
+    let message = message.into_my_slack().await;
     let message = message.render_template();
 
     let post_chat_req = if let Some(thread_ts) = parent {
         SlackApiChatPostMessageRequest::new("#aaron-test-channel".into(), message)
-            .with_thread_ts(thread_ts)
+            .with_thread_ts(thread_ts.clone())
     } else {
         SlackApiChatPostMessageRequest::new("#aaron-test-channel".into(), message)
     };
