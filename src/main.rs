e38b993a500df495b5700098ed9b43d4571cebe1
Hellymaw <aaron@helmore.org>
2024-05-25 23:15:58 +1000

    Persist info about Slack message threads via db

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,15 @@
-use axum::{extract::Json, extract::State, routing::post, Router};
+use axum::Extension;
+use axum::{extract::Json, routing::post, Router};
 use gitea_webhooks::Webhook;
 use serde_json;
 use slack_morphism::prelude::*;
-use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
+use sqlx::postgres::PgPool;
 use tower_http::trace::TraceLayer;
 use tracing;
 use tracing_appender;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 pub mod gitea_webhooks;
-
-#[derive(Default, Debug)]
-pub struct AppState {
-    pub slack_message_cache: HashMap<String, SlackTs>,
-    pub slack_user_lookup: HashMap<String, String>,
-}
-
-pub type SharedState = Arc<Mutex<AppState>>;
 
 #[tokio::main]
 async fn main() {
@@ -38,47 +30,67 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer().with_writer(non_blocking))
         .init();
 
-    let state = SharedState::default();
+    let db_conn_string =
+        std::env::var("DB_CONNECTION_STRING").expect("A database connection string is required");
+    let db_pool = PgPool::connect(&db_conn_string).await.unwrap();
 
     let app = Router::new()
         .route("/", post(post_handler))
         .layer(TraceLayer::new_for_http())
-        .with_state(state.clone());
+        .layer(Extension(db_pool));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:4242").await.unwrap();
 
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn post_handler(State(state): State<SharedState>, Json(payload): Json<serde_json::Value>) {
+async fn post_handler(db: Extension<PgPool>, Json(payload): Json<serde_json::Value>) {
     tracing::debug!(%payload);
 
     match serde_json::from_value::<Webhook>(payload) {
-        Ok(webhook) => post_repo_payload(webhook, state).await,
+        Ok(webhook) => post_repo_payload(webhook, db).await,
         Err(x) => tracing::error!("Error decoding JSON payload into Webhook \"{}\"", x),
     }
 }
 
-async fn post_repo_payload(payload: Webhook, state: SharedState) {
+async fn post_repo_payload(payload: Webhook, db: Extension<PgPool>) {
     let payload = payload.try_deanonymise_emails().await;
 
     let ts = {
-        let state_data = state.lock().unwrap();
-        state_data
-            .slack_message_cache
-            .get(&payload.pull_request.url.as_str().to_string())
-            .map(|ts| ts.clone())
+        let rows: Result<Option<(String,)>, sqlx::Error> =
+            sqlx::query_as("SELECT ts FROM threads WHERE url = $1")
+                .bind(payload.pull_request.url.to_string())
+                .fetch_optional(&*db)
+                .await;
+
+        match rows {
+            Ok(rows) => rows.map(|row| SlackTs::new(row.0)),
+            Err(x) => {
+                tracing::error!(
+                    "Error attempting to retrieve possible timestamp from DB: \"{}\"",
+                    x
+                );
+                None
+            }
+        }
     };
 
     let response = payload.post_slack_message(&ts).await;
     if ts.is_none() {
         if let Ok(response) = response {
-            let mut state_data = state.lock().unwrap();
-            state_data
-                .slack_message_cache
-                .entry(payload.pull_request.url.as_str().to_string())
-                .or_insert(response);
+            let resp = sqlx::query("INSERT INTO threads VALUES ($1, $2)")
+                .bind(payload.pull_request.url.as_str())
+                .bind(&response.0)
+                .execute(&*db)
+                .await;
 
-            tracing::info!("Top level Slack Thread created");
+            if let Err(x) = resp {
+                tracing::error!(
+                    "Error attempting to add a new timestamp to the DB: \"{}\"",
+                    x
+                )
+            } else {
+                tracing::info!("Top level Slack Thread created");
+            }
         }
     }
 }
